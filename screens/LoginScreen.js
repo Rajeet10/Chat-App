0e01c2adcc14f0bb64856029d65b349d64331b4d
Rajeet <rajeetdps@gmail.com>
2021-08-14 19:48:06 +0530

    Added logo on login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import { View, Text,StyleSheet } from 'react-native'
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, Text,StyleSheet } from 'react-native';
+import {Button,Input,Image} from 'react-native-elements';
 
 const LoginScreen = () => {
     return (
         <View>
-            <Text>Login Screen</Text>
+            <StatusBar style="light" />
+            <Image 
+            source={{
+               uri:
+               "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Signal-Logo.svg/1200px-Signal-Logo.svg.png"
+            }}
+            style={{width:200 , height:200}}
+            />
         </View>
     )
 }

40c5901db6b284d7906029348e4fae8cb3044d2a
Rajeet <rajeetdps@gmail.com>
2021-08-14 20:10:20 +0530

    Adding rehgister screen on stack

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
 import { createStackNavigator } from '@react-navigation/stack';
+import RegisterScreen from './screens/RegisterScreen';
 
 const Stack=createStackNavigator();
 const globalScreenOptions={
@@ -20,6 +21,10 @@ export default function App() {
       <Stack.Screen 
       name="Login"
       component={LoginScreen} />
+      <Stack.Screen
+      name="Register"
+      component={RegisterScreen}
+      />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const RegisterScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Text>Register Screen</Text>
+        </View>
+    )
+}
+
+export default RegisterScreen;
+
+const styles=StyleSheet.create({
+
+});

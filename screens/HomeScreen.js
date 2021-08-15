75be4819420edf07ff97ef49648960d4cee25c2b
Rajeet <rajeetdps@gmail.com>
2021-08-15 21:38:34 +0530

    Adding new chat to chatsscreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect , useState } from 'react'
 import { SafeAreaView } from 'react-native';
 import { View, Text, StyleSheet,ScrollView,TouchableOpacity } from 'react-native'
 import { Avatar } from 'react-native-elements';
@@ -7,12 +7,21 @@ import { auth,db } from '../firebase';
 import {AntDesign,SimpleLineIcons} from '@expo/vector-icons';
 
 const HomeScreen = ({navigation}) => {
-
+     const [chats, setChats] = useState([]);
     const signOutUser=()=>{
         auth.signOut().then(()=>{
             navigation.replace("Login");
         });
     };
+    useEffect(()=>{
+     const unsubscribe=db.collection('chats').onSnapshot(snapshot=>{
+      setChats(snapshot.docs.map(doc=>({
+          id:doc.id,
+          data:doc.data()
+      })))
+     })
+     return unsubscribe;
+    },[])
     useLayoutEffect(()=>{
         navigation.setOptions({
             title:"Signal",
@@ -53,7 +62,10 @@ const HomeScreen = ({navigation}) => {
     return (
         <SafeAreaView>
             <ScrollView>
-                <CustomListItem/>
+                {chats.map(({ id,data:{chatName}} )=>{
+                    <CustomListItem/>
+                })}
+                
             </ScrollView>
         </SafeAreaView>
     )

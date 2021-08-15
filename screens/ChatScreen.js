202ecd25df0d3e7b9de180479fcaf419ee60a0ac
Rajeet <rajeetdps@gmail.com>
2021-08-15 22:25:12 +0530

    Building chat view

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,9 @@
 import React, { useLayoutEffect } from 'react'
-import { View, Text, TouchableOpacity,SafeAreaView } from 'react-native'
+import { View, Text, TouchableOpacity,SafeAreaView,KeyboardAvoidingView,Platform,StyleSheet,ScrollView } from 'react-native'
 import { Avatar } from 'react-native-elements'
 import {AntDesign,FontAwesome,Ionicons} from '@expo/vector-icons'
+import { StatusBar } from 'expo-status-bar'
+
 
 const ChatScreen = ({ navigation,route }) => {
     useLayoutEffect(()=>{
@@ -60,10 +62,29 @@ const ChatScreen = ({ navigation,route }) => {
       });
     },[navigation])
     return (
-        <SafeAreaView>
-            <Text>{route.params.chatName}</Text>
+        <SafeAreaView style={{flex:1, backgroundColor:"white"}}>
+            <StatusBar style="light"/>
+            <KeyboardAvoidingView
+            behavior={Platform.OS==="ios" ? "padding" :"height"}
+            style={styles.container}
+            keyboardVerticalOffset={90}
+            >
+             <>
+             <ScrollView>
+                 {/* chat goes here */}
+             </ScrollView>
+             <View style={styles.footer}>
+
+             </View>
+             </>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
 
-export default ChatScreen
+export default ChatScreen;
+
+const styles=StyleSheet.create({
+container:{},
+footer:{}
+})

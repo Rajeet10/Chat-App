d264f38581faac823d8c980e8759a484307bd9dd
Rajeet <rajeetdps@gmail.com>
2021-08-15 22:35:19 +0530

    Completeed the send message layout screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -62,7 +62,11 @@ const ChatScreen = ({ navigation,route }) => {
           </View>
       )
       });
-    },[navigation])
+    },[navigation]);
+
+    const sendMessage=()=>{
+
+    }
     return (
         <SafeAreaView style={{flex:1, backgroundColor:"white"}}>
             <StatusBar style="light"/>
@@ -82,6 +86,11 @@ const ChatScreen = ({ navigation,route }) => {
               placeholder="Signal Message" 
               style={styles.textInput}
               />
+              <TouchableOpacity 
+              onPress={sendMessage} 
+              activeOpacity={0.5}>
+               <Ionicons name="send" size={24} color="#2B68E6"/>
+              </TouchableOpacity>
              </View>
              </>
             </KeyboardAvoidingView>
@@ -92,6 +101,26 @@ const ChatScreen = ({ navigation,route }) => {
 export default ChatScreen;
 
 const styles=StyleSheet.create({
-container:{},
-footer:{}
+container:{
+    flex:1,
+
+},
+footer:{
+    flexDirection:'row',
+    alignItems:'center',
+    width:"100%",
+    padding:15
+},
+textInput:{
+    bottom:0,
+    height:40,
+    flex:1,
+    marginRight:15,
+    borderColor:"transparent",
+    backgroundColor:'#ECECEC',
+    borderWidth:1,
+    padding:10,
+    color:'grey',
+    borderRadius:30
+}
 })

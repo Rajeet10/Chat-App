120e6e95cc0901986d9363d016926b5afbc4845c
Rajeet <rajeetdps@gmail.com>
2021-08-15 22:16:55 +0530

    Added and styled the header in chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,68 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { View, Text, TouchableOpacity,SafeAreaView } from 'react-native'
+import { Avatar } from 'react-native-elements'
+import {AntDesign,FontAwesome,Ionicons} from '@expo/vector-icons'
 
 const ChatScreen = ({ navigation,route }) => {
+    useLayoutEffect(()=>{
+      navigation.setOptions({
+      title:"Chat",
+      headerBackTitleVisible:false,
+      headerTitleAlign:"left",
+      headerTitle:()=>(
+          <View
+          style={{
+              flexDirection:'row',
+              alignItems:'center'
+          }}
+          >
+              <Avatar
+              rounded
+              source={{
+                  uri:
+                  "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+              }}
+              />
+              <Text style={{color:'white',marginLeft:10,fontWeight:"700"}}>
+                  {route.params.chatName}
+              </Text>
+          </View>
+      ),
+      headerLeft:()=>(
+          <TouchableOpacity
+          style={{marginLeft:10}}
+          onPress={navigation.goBack}
+          >
+           <AntDesign name="arrowleft" size={24} color="white"/>
+          </TouchableOpacity>
+      ),
+      headerRight:()=>(
+          <View
+          style={{
+              flexDirection:'row',
+              justifyContent:'space-between',
+              width:80,
+              marginRight:20
+          }}
+          >
+              <TouchableOpacity>
+                  <FontAwesome 
+                  name="video-camera"
+                  size={24}
+                  color="white"
+                  />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                  <Ionicons name="call" size={24} color="white" />
+              </TouchableOpacity>
+          </View>
+      )
+      });
+    },[navigation])
     return (
-        <View>
+        <SafeAreaView>
             <Text>{route.params.chatName}</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
